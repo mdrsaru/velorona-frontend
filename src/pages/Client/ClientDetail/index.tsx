2385dde52dd3470590bbbe400caace45aca54a4d
sarumdr1 <msaru@insightworkshop.io>
2022-05-19 11:09:06 +0545

    Designed client detail modal component

---
+++ src/pages/Client/ClientDetail/index.tsx
@@ -0,0 +1,47 @@
+import { Modal } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
+
+import styles from "./style.module.scss";
+import { Client } from "../../../interfaces/generated";
+
+interface IProps {
+  visible: boolean;
+  client: any;
+  setVisible: any;
+}
+
+const ClientDetail = (props: IProps) => {
+  return (
+    <div className={styles["detail-container"]}>
+      <Modal
+        centered
+        visible={props.visible}
+        closeIcon={[
+          <div onClick={() => props.setVisible(false)}>
+            <span className={styles["close-icon-div"]}>
+              <CloseOutlined />
+            </span>
+          </div>,
+        ]}
+        okText="Cancel"
+        cancelButtonProps={{ style: { display: "none" } }}
+        okButtonProps={{ style: { marginBottom: "1rem" } }}
+        onOk={() => props.setVisible(false)}
+        width={700}
+      >
+        <div className={styles["modal-title"]}>
+          <p>Client Information</p>
+        </div>
+        <div className={styles["container"]}>
+          <div className={styles["client-status"]}>{props.client.status}</div>
+          <p className={styles["client-name"]}>{props.client.name}</p>
+          <p className={styles["client-detail"]}>
+            {props.client.address.streetAddress}
+          </p>
+          <p className={styles["client-detail"]}>{props.client.email}</p>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+export default ClientDetail;
