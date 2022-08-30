70035640fd0cb37e721af3c021f353a70aafb1dc
Saru Manandhar <msaru@insightworkshop.io>
2022-08-30 14:45:43 +0545

    Add auto log out functionality

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,17 @@
-import { useQuery } from '@apollo/client';
-import { Outlet, Navigate } from 'react-router-dom';
+import { useApolloClient, useMutation, useQuery } from '@apollo/client';
+import { Outlet, Navigate, useNavigate } from 'react-router-dom';
 import { Layout } from 'antd';
-
+import ms from 'ms';
 import { SIDEBAR } from '../../gql/app.gql';
 import routes from '../../config/routes';
 
 import Sidebar from '../Sidebar';
-import Header from '../Header';
+import Header, { LOGOUT } from '../Header';
 import { sidebarVar, authVar } from "../../App/link";
 import styles from './style.module.scss';
 import Footer from '../Footer';
+import { useIdleTimer } from 'react-idle-timer';
+import config from '../../config/constants';
 
 const { Content } = Layout;
 
@@ -17,6 +19,50 @@ const { Content } = Layout;
 const _Layout = () => {
   const { data: sidebarData } = useQuery(SIDEBAR);
   const authData = authVar();
+  const timeout = ms(config.accessTokenLife);
+
+  const client = useApolloClient();
+
+  const navigate = useNavigate()
+  const [logout] = useMutation(LOGOUT, {
+    onCompleted(data) {
+      authVar({
+        isLoggedIn: false,
+        token: null,
+        user: {
+          roles: [],
+          id: null,
+        },
+        company: {
+          id: null,
+          code: '',
+          name: '',
+          logo: {
+            id: null,
+            name: null,
+            url: null,
+          }
+        },
+        fullName: null,
+        avatar: {
+          id: null,
+          url: '',
+        }
+      });
+      client.clearStore();
+      navigate(routes.login.path);
+    },
+  });
+
+  const handleOnIdle = () => {
+    // Logout if the user is idle for given timeout duration
+    logout()
+  }
+  useIdleTimer({
+    timeout,
+    onIdle: handleOnIdle,
+    crossTab:true,
+  })
 
   const isLoggedIn = authData?.isLoggedIn;
 
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,4 @@
 declare module 'react-pdf';
 declare module 'pdfjs-dist/build/pdf.worker.entry';
 declare module '@stripe/stripe-js';
+declare module 'ms';
